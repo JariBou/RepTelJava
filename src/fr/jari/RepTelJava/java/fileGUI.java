2ec625bb656a27d6891a4cfcdd4a7fb615b7ddaa
Tomé <minefold.tome2242@gmail.com>
2020-11-03 13:43:06 +0100

    French translation \ IMPORTANT: problems in the '2' option {// problem if there are 2 numbers that are the same} NOT FIXED \ Started doing the function documentation Code Cleanup

--- src/fr/jari/RepTelJava/java/fileGUI.java
+++ src/fr/jari/RepTelJava/java/fileGUI.java
@@ -8,27 +8,19 @@ import java.nio.file.Paths;
 
 public class fileGUI extends JFrame {
     private JPanel mainPanel;
-    private JSlider sliderSize;
-    private JLabel sizeLabel;
     public JLabel display;
     private JScrollPane scroll;
 
     Font defaultFont = new Font("Arial", Font.PLAIN, 25);
 
-    public Font setFontSize(int size) { // Changes the font size, used for button
-        return new Font("Arial", Font.PLAIN, size);
-    }
-
     public fileGUI(String filename) {
         display.setFont(defaultFont);
         display.setVisible(true);
-        sizeLabel.setVisible(true);
         Path currentRelativePath = Paths.get("");
         String imPath = currentRelativePath.toAbsolutePath().toString();
         ImageIcon icon = new ImageIcon(imPath + "/icons/mainIcon.png");
         setIconImage(icon.getImage());
         setTitle("RepTel.java");
-        //scroll.setVisible(true);
         add(mainPanel);
         scroll.setViewportView(display);// Set the viewport view only when the panel has been initialized
         scroll.getVerticalScrollBar().setUnitIncrement(10);
@@ -36,18 +28,5 @@ public class fileGUI extends JFrame {
         setTitle(filename);
         setSize(500, 600);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-
-        sliderSize.addChangeListener(e -> { // Detects if the slider state changed
-            JSlider source = (JSlider) e.getSource();
-            if (!source.getValueIsAdjusting()) {
-                int size = source.getValue();
-                display.setFont(setFontSize(size)); // sets the font size according to the value of the slider
-
-            }
-        });
-
-
     }
-
 }
